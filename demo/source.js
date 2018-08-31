e80979d4a67185c368ce5ec8b20af4e3efc07602
vivaxy <xyxuye2007@126.com>
2018-08-31 09:40:16 +0800

    chore:

--- demo/source.js
+++ demo/source.js
@@ -2,43 +2,42 @@
  * @since 2015-09-21 13:50
  * @author vivaxy
  */
-'use strict';
 
 import Tween from '../src/tween.js';
 
 let ele = document.querySelector('.test'),
-    topFrom = 90, topTo = 0, leftFrom = 90, leftTo = 0, vertical, horizon;
+  topFrom = 90, topTo = 0, leftFrom = 90, leftTo = 0, vertical, horizon;
 
 let toggleTop = () => {
-    let temp = topTo;
-    topTo = topFrom;
-    topFrom = temp;
-    vertical = new Tween(ele)
-        .animate({
-            style: 'top',
-            from: topFrom,
-            to: topTo,
-            duration: 1000,
-            getValue: v => v + '%'
-        })
-        .on('end', toggleTop)
-        .start();
+  let temp = topTo;
+  topTo = topFrom;
+  topFrom = temp;
+  vertical = new Tween(ele)
+    .animate({
+      style: 'top',
+      from: topFrom,
+      to: topTo,
+      duration: 1000,
+      getValue: v => v + '%'
+    })
+    .on('end', toggleTop)
+    .start();
 };
 
 let toggleLeft = () => {
-    let temp = leftTo;
-    leftTo = leftFrom;
-    leftFrom = temp;
-    horizon = new Tween(ele)
-        .animate({
-            style: 'left',
-            from: leftFrom,
-            to: leftTo,
-            duration: 1357,
-            getValue: (v) => v + '%'
-        })
-        .on('end', toggleLeft)
-        .start();
+  let temp = leftTo;
+  leftTo = leftFrom;
+  leftFrom = temp;
+  horizon = new Tween(ele)
+    .animate({
+      style: 'left',
+      from: leftFrom,
+      to: leftTo,
+      duration: 1357,
+      getValue: (v) => v + '%'
+    })
+    .on('end', toggleLeft)
+    .start();
 };
 
 toggleTop();
--- src/tween.js
+++ src/tween.js
@@ -34,80 +34,80 @@ import EventEmitter from 'event-emitter/src/event-emitter.js';
  */
 class Tween extends EventEmitter {
 
-    constructor(element) {
-        super(arguments);
-        this.element = element;
-        // this.style = undefined;
-        // this.from = undefined;
-        // this.to = undefined;
-        // this.duration = undefined;
-        // this.getValue = undefined;
+  constructor(element) {
+    super(arguments);
+    this.element = element;
+    // this.style = undefined;
+    // this.from = undefined;
+    // this.to = undefined;
+    // this.duration = undefined;
+    // this.getValue = undefined;
 
-        // this._beginTime = 0;
-        this._events = {};
-        this._animating = false;
-    }
+    // this._beginTime = 0;
+    this._events = {};
+    this._animating = false;
+  }
 
-    /**
-     *
-     * @param options
-     * @returns {Tween}
-     */
-    animate(options) {
-        this.style = options.style;
-        this.from = options.from;
-        this.to = options.to;
-        this.duration = options.duration;
-        this.getValue = options.getValue || function(v) {
-                return v;
-            };
-        return this;
-    }
+  /**
+   *
+   * @param options
+   * @returns {Tween}
+   */
+  animate(options) {
+    this.style = options.style;
+    this.from = options.from;
+    this.to = options.to;
+    this.duration = options.duration;
+    this.getValue = options.getValue || function(v) {
+      return v;
+    };
+    return this;
+  }
 
-    /**
-     * play it
-     * @returns {Tween}
-     */
-    start() {
-        if (!this._animating) {
-            this._animating = true;
-            this._beginTime = new Date().getTime();
-            this._loop();
-        }
-        return this;
+  /**
+   * play it
+   * @returns {Tween}
+   */
+  start() {
+    if (!this._animating) {
+      this._animating = true;
+      this._beginTime = new Date().getTime();
+      this._loop();
     }
+    return this;
+  }
 
-    /**
-     *
-     * @returns {Tween}
-     * @private
-     */
-    _loop() {
-        let now = new Date().getTime();
-        if (this._animating) {
-            if (now < this._beginTime + this.duration) {
-                let value = this.from + (this.to - this.from) * (now - this._beginTime) / this.duration;
-                this.element.style[this.style] = this.getValue(value);
-                this.emit('frame', value);
-                window.requestAnimationFrame(this._loop.bind(this));
-            } else {
-                // make sure final style
-                this.element.style[this.style] = this.getValue(this.to);
-                this.emit('end');
-            }
-        }
+  /**
+   *
+   * @returns {Tween}
+   * @private
+   */
+  _loop() {
+    let now = new Date().getTime();
+    if (this._animating) {
+      if (now < this._beginTime + this.duration) {
+        let value = this.from + (this.to - this.from) * (now - this._beginTime) / this.duration;
+        this.element.style[this.style] = this.getValue(value);
+        this.emit('frame', value);
+        window.requestAnimationFrame(this._loop.bind(this));
+      } else {
+        // make sure final style
+        this.element.style[this.style] = this.getValue(this.to);
+        this.emit('end');
+      }
     }
+  }
 
-    /**
-     * pause
-     */
-    pause() {
-        this._animating = false;
-        let elapsed = new Date().getTime() - this._beginTime;
-        this.from = this.from + (this.to - this.from) * elapsed / this.duration;
-        this.duration = this.duration - elapsed;
-        return this;
-    }
+  /**
+   * pause
+   */
+  pause() {
+    this._animating = false;
+    let elapsed = new Date().getTime() - this._beginTime;
+    this.from = this.from + (this.to - this.from) * elapsed / this.duration;
+    this.duration = this.duration - elapsed;
+    return this;
+  }
 
 }
 
